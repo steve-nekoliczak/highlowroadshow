ffa2fdc08f2cfba5a39dfe8b29b205d4d4b88139
Steve Nekoliczak <steve.nekoliczak@protonmail.com>
2025-12-24 10:57:58 -0600

    Add user authentication
    Used this command:
    
    ```
    mix phx.gen.auth Accounts User users
    ```

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :highlowroadshow, :scopes,
+  user: [
+    default: true,
+    module: Highlowroadshow.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: Highlowroadshow.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :highlowroadshow,
   ecto_repos: [Highlowroadshow.Repo],
   generators: [timestamp_type: :utc_datetime]
